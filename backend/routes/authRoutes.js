18157658f8a4411a3b57bdb24753ec810c904849
Pedro Delgado <pedro@pedrodelgado.ca>
2023-03-15 14:49:00 -0400

    initial commit
    initial commit

---
+++ backend/routes/authRoutes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const authControllers = require('../controllers/auth/authControllers');
+
+router.post("/register", authControllers.controllers.postRegister);
+router.post("/login", authControllers.controllers.postLogin);
+
+
+module.exports = router;
